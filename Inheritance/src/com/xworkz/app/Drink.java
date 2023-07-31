d810f716f7d0cc407201b74bd65c98b36d67fb38
Manojtj8055 <manutj805517@gmail.com>
2023-07-30 17:17:53 -0700

    Inheritance

---
+++ Inheritance/src/com/xworkz/app/Drink.java
@@ -0,0 +1,30 @@
+package com.xworkz.app;
+
+public class Drink {
+
+	String type;
+	int brand;
+	
+	
+	public Drink(String type, int brand) {
+		
+		this.type = type;
+		this.brand = brand;
+		
+		
+	System.out.println("Features of a drink");
+	}
+	
+	
+
+
+}
+
+
+
+	
+	
+	
+	
+	
+
---
+++ Inheritance/src/com/xworkz/app/King.java
@@ -0,0 +1,27 @@
+package com.xworkz.app;
+
+public class King {
+
+	
+	String Name;
+	int Rules;
+	boolean Justice;
+	public King(String name, int rules, boolean justice) {
+		
+		this.Name = name;
+		this.Rules = rules;
+		this.Justice = justice;
+	}
+	
+
+public void printinfo() {
+		
+		System.out.println("The King name is " + Name );
+		System.out.println("King Rules " + Rules + " states");
+		System.out.println("Justice = " + Justice);
+		System.out.println("");
+		System.out.println("+++++++++++++++++++++++++++++++++++");
+		System.out.println("");
+	
+}
+}
